37167851100c953743b7828825dc1ee120fd859c
Swarleys <elessar29@gmail.com>
2019-06-12 22:11:06 +0200

    Adding Navbar, Footer and Go back button to SingleColorPalette

--- src/Palette.js
+++ src/Palette.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
+import PaletteFooter from "./PaletteFooter";
 import "./Palette.css";
 
 export default class Palette extends Component {
@@ -40,12 +41,10 @@ export default class Palette extends Component {
           level={level}
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
+          showingAllColors
         />
         <div className="Palette-colors">{colorBoxes}</div>
-        <footer className="palette-footer">
-          {paletteName}
-          <span className="emoji">{emoji}</span>
-        </footer>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -21,19 +21,19 @@ const styles = {
     display: "flex",
     width: "100%",
     justifyContent: "space-between",
-    color: 'white'
+    color: "white"
   },
   palettes: {
-    boxSizing: 'border-box',
-    width: '100%',
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%'
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%"
   }
 };
 
 class PaletteList extends Component {
-  goToPalette(id){
+  goToPalette(id) {
     this.props.history.push(`/palette/${id}`);
   }
   render() {
@@ -47,7 +47,11 @@ class PaletteList extends Component {
           </nav>
           <div className={classes.palettes}>
             {palettes.map(palette => (
-              <MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)} />
+              <MiniPalette
+                {...palette}
+                handleClick={() => this.goToPalette(palette.id)}
+                key={palette.paletteName}
+              />
             ))}
           </div>
         </div>
