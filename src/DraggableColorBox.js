ee7c24373f36c93c22ba2583e6ec75ac019f1e66
Swarleys <elessar29@gmail.com>
2019-06-23 18:26:15 +0200

    Moving JSS styles out of components and adding constants.js

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,47 +1,16 @@
 import React from "react";
-import { SortableElement } from 'react-sortable-hoc';
+import { SortableElement } from "react-sortable-hoc";
 import { withStyles } from "@material-ui/styles";
 import DeleteIcon from "@material-ui/icons/Delete";
+import styles from "./styles/DraggableColorBoxStyles";
 
-const styles = {
-  root: {
-    width: "20%",
-    height: "25%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-3.5px",
-    "&:hover svg": {
-      color: "white",
-      transform: 'scale(1.4)'
-    }
-  },
-  boxContent: {
-    position: "absolute",
-    width: "100%",
-    left: 0,
-    bottom: 0,
-    padding: "10px",
-    color: "black",
-    letterSpacing: "1px",
-    textTransform: "uppercase",
-    fontSize: "12px",
-    display: "flex",
-    justifyContent: "space-between"
-  },
-  deleteIcon: {
-    color: "rgba(0,0,0,.5)",
-    transition: 'all .3s ease-in-out'
-  }
-};
-const DraggableColorBox = SortableElement((props) => {
+const DraggableColorBox = SortableElement(props => {
   const { color, name, classes, handleClick } = props;
   return (
     <div className={classes.root} style={{ backgroundColor: color }}>
       <div className={classes.boxContent}>
         <span>{name}</span>
-        <DeleteIcon className={classes.deleteIcon} onClick={handleClick}/>
+        <DeleteIcon className={classes.deleteIcon} onClick={handleClick} />
       </div>
     </div>
   );
